c940b49d65ff520e7418c6224843889fe1bed2c6
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2024-09-22 14:02:43 +0300

    adding react email

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,23 +1,33 @@
+import { TRPCError } from "@trpc/server";
+import GalleryStudioVerifyOTPEmail from "react-email-starter/emails/gallery-studio-verify-otp";
 import { Resend } from "resend";
 import { EmailTemplate } from "~/app/_components/EmailTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function Send(userName: string, otp: string, email:string) {
+export async function Send(userName: string, OTPCode: string, email: string) {
   try {
     const { data, error } = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: email,
       subject: "Gallery-Studio OTP",
-      react: EmailTemplate({ userName, otp }),
+      react: GalleryStudioVerifyOTPEmail({ userName, OTPCode }),
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to send email",
+        cause: error,
+      });
     }
 
-    return Response.json(data);
+    return data;
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An unexpected error occurred",
+      cause: error,
+    });
   }
 }
