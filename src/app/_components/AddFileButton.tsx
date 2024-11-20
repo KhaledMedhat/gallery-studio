e3af1701da8775794affc8b317a560cb54a5b52f
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2024-11-20 18:55:00 +0200

    adding add file to choose files modal and disable choose from gallery button if no files selected

--- src/app/_components/AddFileButton.tsx
+++ src/app/_components/AddFileButton.tsx
@@ -42,12 +42,16 @@ import { useTheme } from "next-themes";
 import type { fileType } from "~/types/types";
 import Video from "./Video";
 import { Switch } from "~/components/ui/switch";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 
 const AddFileButton: React.FC<{
   files?: fileType[] | undefined;
-  isEmptyPage: boolean;
+  isEmptyPage?: boolean;
+  albumId?: string;
   gallerySlug: string;
-}> = ({ files, gallerySlug, isEmptyPage }) => {
+  isTabs?: boolean;
+}> = ({ files, gallerySlug, isEmptyPage, isTabs, albumId }) => {
+  console.log(gallerySlug)
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const [file, setFile] = useState<File | undefined>(undefined);
   const [isFileError, setIsFileError] = useState<boolean>(false);
@@ -94,8 +98,24 @@ const AddFileButton: React.FC<{
       privacy: false
     },
   });
-  const { mutate: addFile, isPending } = api.file.addFile.useMutation({
+  const { mutate: addToExistedAlbum, isPending: isAddToExistingAlbumPending } = api.album.addToAlbum.useMutation({
     onSuccess: () => {
+      toast({
+        title: "Added Successfully.",
+        description: `Images has been added successfully.`,
+      });
+      void utils.file.getAlbumFiles.invalidate();
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: `Uh oh! Something went wrong. Please try again.`,
+      });
+    },
+  });
+  const { mutate: addFile, isPending } = api.file.addFile.useMutation({
+    onSuccess: (data) => {
       setFile(undefined);
       setFileUrl("");
       setFileKey("");
@@ -103,8 +123,12 @@ const AddFileButton: React.FC<{
       form.reset();
       void utils.file.getFiles.invalidate();
       toast({
-        description: <span>Your Image has been added successfully</span>,
+        description: <span>Your {data?.fileType?.includes('image') ? data.fileType.includes("gif") ? "GIF" : "Image" : "Video"} has been added successfully</span>,
       });
+      if (isTabs && data) {
+        addToExistedAlbum({ id: [data.id], albumId: Number(albumId) })
+
+      }
     },
     onError: (e) => {
       toast({
@@ -129,7 +153,154 @@ const AddFileButton: React.FC<{
       });
     }
   };
+
+
+  if (isTabs) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Add Image , Video or GIF</CardTitle>
+          <CardDescription>
+            Upload a new image , video or even GIF to your gallery. Click save when you&apos;re
+            done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <Form {...form}>
+            <form
+              id="add-image-form"
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-8"
+            >
+              <div className="flex flex-col gap-6">
+                {file && fileUrl && progress === 100 ? (
+                  <div className="relative flex flex-col items-center justify-center gap-6">
+                    {fileType.includes("video") ? (
+                      <Video url={fileUrl} />
+                    ) : (
+                      <AspectRatio ratio={1 / 1}>
+                        <Image
+                          src={fileUrl}
+                          alt="Profile Picture"
+                          layout="fill"
+                          className="h-full w-full cursor-pointer rounded-full object-cover"
+                        />
+                      </AspectRatio>
+                    )}
+                    <Button
+                      className="absolute right-0 top-0"
+                      type="button"
+                      variant="ghost"
+                      onClick={async () => {
+                        if (fileKey) {
+                          await deleteFileOnServer(fileKey);
+                          setFile(undefined);
+                          setFileUrl("");
+                        }
+                      }}
+                    >
+                      <X size={20} />
+                    </Button>
+                  </div>
+                ) : isUploading && progress !== 0 ? (
+                  <AnimatedCircularProgressBar
+                    className="m-auto"
+                    max={100}
+                    min={0}
+                    value={progress}
+                    gaugePrimaryColor={
+                      theme.resolvedTheme === "dark" ? "#d4d4d4" : "#171717"
+                    }
+                    gaugeSecondaryColor={
+                      theme.resolvedTheme === "dark" ? "#171717" : "#d4d4d4"
+                    }
+                  />
+                ) : (
+                  <UploadthingButton
+                    isImageComponent={true}
+                    setFile={setFile}
+                    label={"Image"}
+                    isFileError={isFileError}
+                  />
+                )}
+
+                <FormField
+                  control={form.control}
+                  name="caption"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Caption *</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Caption of the image" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter image caption.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="tags"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Tags</FormLabel>
+                      <FormControl>
+                        <Input placeholder="#tags" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        Enter image tags if you want to add them.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {file &&
+                  <FormField
+                    control={form.control}
+                    name="privacy"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-row items-center gap-1 justify-between rounded-lg border p-3 shadow-sm">
+                        <div className="space-y-0.5">
+                          <FormLabel>{fileType.includes('image') ? "Image Privacy" : "Video Privacy"}</FormLabel>
+                          <FormDescription>
+                            By default it sets to private but you can change it to public.
+                          </FormDescription>
+                        </div>
+                        <FormControl>
+                          <Switch
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                }
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <Button
+            form="add-image-form"
+            type="submit"
+            className="w-full"
+            disabled={isPending || isUploading || isAddToExistingAlbumPending}
+          >
+            {!isPending || !isAddToExistingAlbumPending || !isUploading ? (
+              "Save"
+            ) : (
+              <LoaderCircle size={20} className="animate-spin" />
+            )}
+          </Button>
+        </CardFooter>
+      </Card>
+
+    )
+  }
+
   return (
+
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <TooltipProvider>
         <Tooltip>
@@ -157,7 +328,6 @@ const AddFileButton: React.FC<{
             done.
           </DialogDescription>
         </DialogHeader>
-
         <Form {...form}>
           <form
             id="add-image-form"
@@ -271,6 +441,7 @@ const AddFileButton: React.FC<{
             </div>
           </form>
         </Form>
+
         <DialogFooter>
           <Button
             form="add-image-form"
--- src/app/_components/ChooseFilesModal.tsx
+++ src/app/_components/ChooseFilesModal.tsx
@@ -2,9 +2,10 @@
 import { Earth, Images, LoaderCircle, LockKeyhole } from "lucide-react"
 import { Badge } from "~/components/ui/badge"
 import BlurFade from "~/components/ui/blur-fade"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 import { Button } from "~/components/ui/button"
-import { Card, CardContent } from "~/components/ui/card"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog"
 import { api } from "~/trpc/react"
 import Video from "./Video"
 import Image from "next/legacy/image"
@@ -12,6 +13,8 @@ import { Checkbox } from "~/components/ui/checkbox"
 import { useState } from "react"
 import { toast } from "~/hooks/use-toast"
 import { useParams } from "next/navigation"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import AddFileButton from "./AddFileButton";
 
 const ChooseFilesModal: React.FC<{ isInsideAlbum?: boolean }> = ({ isInsideAlbum }) => {
     const param = useParams()
@@ -48,80 +51,97 @@ const ChooseFilesModal: React.FC<{ isInsideAlbum?: boolean }> = ({ isInsideAlbum
                 </Button>
             </DialogTrigger>
             <DialogContent className="h-fit max-h-full overflow-y-auto">
-                <DialogHeader>
-                    <DialogTitle>Add from your gallery</DialogTitle>
-                    <DialogDescription>
-                        You can add images, videos even GIF from your gallery into this album by selecting them.
-                    </DialogDescription>
-                </DialogHeader>
-                <Card className="max-h-[80vh] w-full overflow-y-auto">
-                    <CardContent className="flex gap-4 p-6 flex-wrap justify-center">
-                        {files?.map((file, idx) => (
-                            <BlurFade key={file.id} delay={0.25 + Number(idx) * 0.05} inView>
-                                <div className={`group flex flex-col gap-1 relative h-full w-full overflow-hidden rounded-lg`}>
-                                    <Checkbox
-                                        disabled={isTheFileInAlbum(file.id)}
-                                        onCheckedChange={(checked) => {
-                                            if (checked) {
-                                                setSelectedFiles((prev) => [...prev, file.id]);
-                                            } else {
-                                                setSelectedFiles((prev) => prev.filter(id => id !== file.id))
-                                            }
-                                        }}
-                                        className={`absolute right-2 top-2 z-10 items-center justify-center bg-muted  `}
-                                    />
-                                    <div className="relative h-full w-full">
-                                        <div className="w-[240px] h-[300px]">
+                <VisuallyHidden.Root>
+                    <DialogHeader>
+                        <DialogTitle></DialogTitle>
+                        <DialogDescription></DialogDescription>
+                    </DialogHeader>
+                </VisuallyHidden.Root>
+                <Tabs defaultValue="Albums" className="w-full">
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="choose-from-gallery">Choose from your gallery</TabsTrigger>
+                        <TabsTrigger value="add-file">Add</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="choose-from-gallery">
+                        <Card className="max-h-[80vh] w-full overflow-y-auto">
+                            <CardHeader>
+                                <CardTitle>Add from your gallery</CardTitle>
+                                <CardDescription>
+                                    You can add images, videos even GIF from your gallery into this album by selecting them.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="flex gap-4 p-6 flex-wrap justify-center">
+                                {files?.map((file, idx) => (
+                                    <BlurFade key={file.id} delay={0.25 + Number(idx) * 0.05} inView>
+                                        <div className={`group flex flex-col gap-1 relative h-full w-full overflow-hidden rounded-lg`}>
+                                            <Checkbox
+                                                disabled={isTheFileInAlbum(file.id)}
+                                                onCheckedChange={(checked) => {
+                                                    if (checked) {
+                                                        setSelectedFiles((prev) => [...prev, file.id]);
+                                                    } else {
+                                                        setSelectedFiles((prev) => prev.filter(id => id !== file.id))
+                                                    }
+                                                }}
+                                                className={`absolute right-2 top-2 z-10 items-center justify-center bg-muted  `}
+                                            />
+                                            <div className="relative h-full w-full">
+                                                <div className="w-[240px] h-[300px]">
 
-                                            {file.fileType?.includes('image') ?
-                                                <Image
-                                                    priority
-                                                    src={file.url}
-                                                    alt={`Gallery image ${file.id}`}
-                                                    layout="fill"
-                                                    className={`h-full w-full shadow rounded-md object-cover transition-transform duration-300 hover:scale-105`}
+                                                    {file.fileType?.includes('image') ?
+                                                        <Image
+                                                            priority
+                                                            src={file.url}
+                                                            alt={`Gallery image ${file.id}`}
+                                                            layout="fill"
+                                                            className={`h-full w-full shadow rounded-md object-cover transition-transform duration-300 hover:scale-105`}
 
-                                                />
-                                                :
-                                                <Video url={file.url} showInitialPlayButton={false} />
-                                            }
+                                                        />
+                                                        :
+                                                        <Video url={file.url} showInitialPlayButton={false} />
+                                                    }
 
+                                                </div>
+                                            </div>
+                                            <div className="w-full flex items-center justify-between px-2">
+                                                <div>
+                                                    {file.fileType?.includes("image") ? (
+                                                        <Badge>
+                                                            {file.fileType.includes("gif") ? "GIF" : "Image"}
+                                                        </Badge>
+                                                    ) : (
+                                                        <Badge>Video</Badge>
+                                                    )}
+                                                </div>
+                                                <div>
+                                                    {file.filePrivacy === "private" ? (
+                                                        <LockKeyhole size={14} />
+                                                    ) : (
+                                                        <Earth size={14} />
+                                                    )}
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="w-full flex items-center justify-between px-2">
-                                        <div>
-                                            {file.fileType?.includes("image") ? (
-                                                <Badge>
-                                                    {file.fileType.includes("gif") ? "GIF" : "Image"}
-                                                </Badge>
-                                            ) : (
-                                                <Badge>Video</Badge>
-                                            )}
-                                        </div>
-                                        <div>
-                                            {file.filePrivacy === "private" ? (
-                                                <LockKeyhole size={14} />
-                                            ) : (
-                                                <Earth size={14} />
-                                            )}
-                                        </div>
-                                    </div>
-                                </div>
-                            </BlurFade>
-                        ))}
-                    </CardContent>
-                </Card>
-                <DialogFooter>
-                    <Button className="w-full"
-                        disabled={isPending}
-                        onClick={() => addToExistedAlbum({ id: selectedFiles, albumId: Number(param.albumId) })}>
-                        {isPending ? (
-                            <LoaderCircle size={20} className="animate-spin" />
-                        ) : (
-                            'Add'
-                        )}
-                    </Button>
-                </DialogFooter>
+                                    </BlurFade>
+                                ))}
+                            </CardContent>
+                            <CardFooter>
+                                <Button className="w-full"
+                                    disabled={isPending || selectedFiles.length === 0}
+                                    onClick={() => addToExistedAlbum({ id: selectedFiles, albumId: Number(param.albumId) })}>
+                                    {isPending ? (
+                                        <LoaderCircle size={20} className="animate-spin" />
+                                    ) : (
+                                        'Add'
+                                    )}
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                    <TabsContent value="add-file">
+                        <AddFileButton isTabs={true} gallerySlug={param.id as string} albumId={param.albumId as string} />
+                    </TabsContent>
+                </Tabs>
             </DialogContent>
         </Dialog>
     )
