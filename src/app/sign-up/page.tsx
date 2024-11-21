98238f861ecbc2080fef33c82c59f056ab05d47d
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2024-11-21 17:15:07 +0200

    adding suspense to sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from "react";
 import SignUp from "../_components/SignUp";
 import { getProvidedUserAccountGallery } from "../actions";
 
 export default async function SignUpPage() {
   await getProvidedUserAccountGallery();
-  return <SignUp />;
+  return (
+    <Suspense fallback={<div className="text-white">Loading...</div>}>
+      <SignUp />;
+    </Suspense>)
 }
