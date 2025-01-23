2357a5cb77524172bde99e119ae1287220720e58
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2025-01-23 16:34:19 +0200

    trying after crash

--- src/app/_components/AuthButtons.tsx
+++ src/app/_components/AuthButtons.tsx
@@ -13,7 +13,7 @@ const AuthButtons = () => {
           }
         >
           <Image src={"/google.svg"} alt="google_icon" width={25} height={25} />
-          Google
+          Google nwe asd
         </Button>
         <Button
           className="flex w-full transform items-center gap-2 rounded-md bg-gradient-to-r from-gray-700 to-gray-900 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:scale-105 hover:from-gray-800 hover:to-black focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
