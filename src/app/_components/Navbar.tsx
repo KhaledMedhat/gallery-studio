dbfd07e9c39f4e7b28809c93572afd1ffd532156
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2025-03-01 23:27:18 +0200

    fix the position of navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC<{ currentUser: User | null | undefined }> = ({
   return (
 
     <header
-      className="flex container w-full md:w-3/4 px-8  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-40 py-2  items-center justify-center space-x-4">
+      className="flex container w-full md:w-3/4 px-8 fixed top-4 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-40 py-2  items-center justify-center space-x-4">
       <div className="w-full flex items-center gap-4">
         <Logo />
         {navItems.map((navItem, idx) => (
