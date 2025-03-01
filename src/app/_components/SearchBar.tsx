7bfc13c5974a4c5ed114f4360281287f1b337151
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2025-03-01 23:25:21 +0200

    width of searchbar adjustment

--- src/app/_components/SearchBar.tsx
+++ src/app/_components/SearchBar.tsx
@@ -71,7 +71,7 @@ const SearchBar = () => {
         transition={{
           duration: 0.2,
         }}
-        className="container w-full md:w-1/4 fixed top-2 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-md dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000]"
+        className="container w-3/4 md:w-1/4 fixed top-2 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-md dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000]"
       >
         <Button variant="ghost" className="w-full p-0 justify-start pl-2" onClick={() => setIsDialogOpen(true)}>Search for Artists or Tags ...</Button>
 
